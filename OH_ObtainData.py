78347ff0c2992a4ffe8efc64515f94f02defce70
Wilson Tobar <53382319+wilsont-2048@users.noreply.github.com>
2022-12-26 18:52:41 -0800

    Update OH_ObtainData.py
    Added waiting messages and working directory paths to save data files.

--- OH_ObtainData.py
+++ OH_ObtainData.py
@@ -1,13 +1,20 @@
+import os
 import sys
 import cloudscraper
 
 
 def get_sos_data():
+    # Get current working DIR and create DATA folder to store data files.
+    cwd = os.getcwd()
+    os.mkdir(cwd + '/DATA')
+
     # Base URL to access data from OH SOS site.
     base_url = "https://www6.ohiosos.gov/ords/f?p=VOTERFTP:DOWNLOAD::FILE:NO:2:P2_PRODUCT_NUMBER:"
 
     # Create instance of web scrapper capable of bypassing Cloudflare anti-bot defense.
-    scrapper = cloudscraper.create_scraper(delay=6, browser='chrome')
+    scrapper = cloudscraper.create_scraper(delay=10, browser='chrome')
+
+    print("Obtaining data from site...")
 
     # Set range to obtain individual data from 88 Ohio counties.
     for i in range(1, 89):
@@ -18,6 +25,8 @@ def get_sos_data():
         # Write file to 'DATA' folder.
         with open(f'./DATA/data{i}.txt', 'wb') as file:
             file.write(data)
+
+    print("Process Complete.")
 
 
 def main():
